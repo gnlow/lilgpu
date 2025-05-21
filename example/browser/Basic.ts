34373ac02308c98a496fcc265f0335d18924ca2e
Gnlow <gnlow@pm.me>
2025-05-21 15:00:42 +0900

    seems work

---
+++ example/browser/Basic.ts
@@ -0,0 +1,62 @@
+const vertShader = `
+struct Output {
+    @builtin(position) pos: vec4f,
+    @location(0) uv: vec2f,
+}
+
+@vertex
+fn main(
+    @builtin(vertex_index) vertexIndex: u32,
+) -> Output {
+    var pos = array<vec2f, 4>(
+        vec2(1, 1), // top-right
+        vec2(-1, 1), // top-left
+        vec2(1, -1), // bottom-right
+        vec2(-1, -1) // bottom-left
+    );
+    var uv = array<vec2f, 4>(
+        vec2(1., 1.), // top-right
+        vec2(0., 1.), // top-left
+        vec2(1., 0.), // bottom-right
+        vec2(0., 0.) // bottom-left
+    );
+    var out: Output;
+    out.pos = vec4f(pos[vertexIndex], 0.0, 1.0);
+    out.uv = uv[vertexIndex];
+    return out;
+}
+`
+const fragShader = `
+@fragment
+fn main(
+    @location(0) uv: vec2f,
+) -> @location(0) vec4f {
+    let red = floor(uv.x * f32(span.x)) / f32(span.x);
+    let green = floor(uv.y * f32(span.y)) / f32(span.y);
+    return vec4(red, green, blue, 1.0);
+}
+`
+import { initCanvas, d } from "../../browser.ts"
+
+import { Lil, uniform } from "../../src/Lil.ts"
+
+class Basic extends Lil {
+    vertShader = vertShader
+    fragShader = fragShader
+
+    @uniform(d.struct({
+        x: d.u32,
+        y: d.u32,
+    }))
+    accessor span = { x: 10, y: 20 }
+
+    @uniform(d.f32)
+    accessor blue = 0.5
+}
+
+const basic = new Basic()
+const g = await basic.initCanvas(document.querySelector("canvas")!)
+
+basic.span = { x: 10, y: 20 }
+basic.blue = 0.5
+g.draw(4)
--- src/Lil.ts
+++ src/Lil.ts
@@ -1,39 +1,78 @@
-// deno-lint-ignore-file no-this-alias no-explicit-any
-import {
-    AnyData,
-    Layout,
-} from "./types.ts"
-import { isWgslData } from "./deps.ts"
-import { initCanvas } from "./GpuWrapper.ts"
+import { Layout } from "./types.ts"
+import { AnyWgslData } from "./deps.ts"
+import { initCanvas } from "./GpuWrapper.ts";
 
-const uniform =
-<T extends AnyData>
-(type: T) =>
-(
-    _: undefined,
-    ctx: ClassFieldDecoratorContext<Lil, T["~repr"]>,
-) => function(this: Lil, value: T["~repr"]) {
-    if (!isWgslData(type)) {
-        throw new Error("Not wgsl data")
+const accessorDecorator =
+<
+    Args extends unknown[],
+    This,
+    Return,
+    _Ctx = ClassAccessorDecoratorContext<This, Return>
+>
+(f: (that: This) => (...args: Args) => {
+    getV(v: Return, ctx: _Ctx): Return,
+    setV(v: Return, ctx: _Ctx): Return,
+    init?(v: Return, ctx: _Ctx): void,
+}) =>
+(...args: Args) => function (
+    this: void,
+    { get, set }: ClassAccessorDecoratorTarget<This, Return>,
+    ctx: _Ctx,
+) {
+    return {
+        get(this: This) {
+            return f(this)(...args).getV(get.call(this), ctx)
+        },
+        set(this: This, newValue: Return) {
+            set.call(this, f(this)(...args).setV(newValue, ctx))
+        },
+        init(this: This, value: Return) {
+            f(this)(...args).init?.(value, ctx)
+            return value
+        },
     }
-    this.layout[ctx.name as string] = { uniform: type }
-
-    return value
 }
+
+export const uniform = 
+<T extends AnyWgslData, C extends Lil>
+(type: T) =>
+accessorDecorator(
+    (lil: C) =>
+    (type: T) => ({
+        getV: (v: T["~repr"]) => v,
+        setV: (v, { name }) => {
+            lil.update(name as any, v as any)
+        },
+        init: (v, { name }) => {
+            lil.layout[name as string] = { uniform: type }
+        },
+    })
+)(type)
+
+type UpdateListener<Class> = <T extends keyof Class>(name: T, value: Class[T]) => void
 
 export abstract class Lil {
     abstract vertShader: string
     abstract fragShader: string
     layout: Layout = {}
 
+    updateListeners: UpdateListener<this>[] = []
+    onUpdate(f: UpdateListener<this>) {
+        this.updateListeners.push(f)
+    }
+
+    update<T extends keyof this>(name: T, value: this[T]) {
+        this.updateListeners.forEach(f => f(name, value))
+    }
+
     async initCanvas(canvas: HTMLCanvasElement) {
-        /*
         const wrapper = await initCanvas({
             ...this,
             canvas,
         })
-            */
-        const wrapper = {}
+        this.onUpdate((name, value) => {
+            (wrapper.buffers as any)[name].write(value)
+        })
 
         const that = this as Record<string, any>
 
@@ -51,18 +90,3 @@ export abstract class Lil {
         })
     }
 }
-
-import * as d from "./deps.ts"
-
-class My extends Lil {
-    vertShader = ""
-    fragShader = ""
-
-    @uniform(d.f32) SEED = 0
-}
-
-const my = new My()
-
-const g1 = await my.initCanvas(0 as any)
-
-console.log(g1.SEED)
