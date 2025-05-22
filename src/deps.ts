e7ea8b91b91aa9fd16b0a7d19097ef3b313aab7a
Gnlow <gnlow@pm.me>
2025-05-22 20:58:54 +0900

    chore: Uniform to UniformFlag

--- src/deps.ts
+++ src/deps.ts
@@ -2,7 +2,7 @@ export {
     default as tgpu,
     type TgpuRoot,
     type TgpuLayoutEntry,
-    type Uniform,
+    type UniformFlag,
 } from "https://esm.sh/typegpu@0.5.3"
 
 export * from "https://esm.sh/typegpu@0.5.3/data"
